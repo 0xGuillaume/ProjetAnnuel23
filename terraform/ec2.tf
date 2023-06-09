a4457a9e35f13e92499d8f37920cfba5e15a1f06
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-06-09 10:39:16 +0200

    Adde tf test

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,46 +1,12 @@
 resource "aws_instance" "esgi_instance" {
   ami                         = "ami-01dd271720c1ba44f"
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.ssh_key.key_name
   subnet_id                   = aws_subnet.esgisub.id
   vpc_security_group_ids      = [aws_security_group.esgi_sg.id]
   associate_public_ip_address = true
 
-  provisioner "file" {
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa.pub")
-      timeout     = "1m"
-      agent       = false
-      host        = self.public_ip
-    }
-
-    source      = ""
-    destination = "/tmp/"
-
-  }
-
-  provisioner "remote-exec" {
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      timeout     = "1m"
-      agent       = false
-      host        = self.public_ip
-    }
-
-    inline = [
-      "sudo apt update",
-      "sudo apt -Y upgrade",
-      "sudo apt-get -y install docker.io",
-      "sudo wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -O /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-      "cd /tmp/",
-      "sudo docker-compose up -d"
-    ]
-  }
+  user_data = <<EOF
+  #!/bin/bash
+  touch /dev/test.txt
+  EOF
 }
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "ssh_key" {
-  key_name   = "esgi_ssh"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
 resource "aws_vpc" "esgi_vpc" {
   cidr_block = "10.0.0.0/16"
 }
