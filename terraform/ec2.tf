a9925d23ca25adcbe05c674c97ab9444b99ea79d
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2023-06-13 21:49:45 +0200

    Removed unused line

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "esgi_instance" {
   vpc_security_group_ids      = [aws_security_group.esgi_sg.id]
   associate_public_ip_address = true
 
-  #user_data = data.cloudinit_config.foobar.rendered
   user_data = <<-EOL
   #!/bin/bash
   sudo -i -u ubuntu bash << EOF
