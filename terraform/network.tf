6556491e31799df7df3145375057a87d82f9db55
Salvor Hardin <65726796+0xGuillaume@users.noreply.github.com>
2023-07-12 14:15:02 +0200

    renamed sg

--- terraform/network.tf
+++ terraform/network.tf
@@ -15,7 +15,7 @@ resource "aws_eip" "ip" {
 }
 
 resource "aws_security_group" "sgroup" {
-  name   = "fooooo_docker_security_group"
+  name   = "foobar_docker_security_group"
   vpc_id = aws_vpc.vpc.id
 
   ingress {
